31b881b84ddd03e9a3de6aed47b1496f1fbc0597
Ying Hsun Chen <stoola20.526@gmail.com>
2022-11-08 20:45:06 +0800

    build: set up push notification and install FirebaseMessaging

--- CookAndShare/AppDelegate.swift
+++ CookAndShare/AppDelegate.swift
@@ -10,6 +10,7 @@ import FirebaseCore
 import GoogleMaps
 import GooglePlaces
 import IQKeyboardManager
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GMSServices.provideAPIKey(APIKey.apiKey)
         GMSPlacesClient.provideAPIKey(APIKey.apiKey)
         IQKeyboardManager.shared().isEnabled = true
+
+        UNUserNotificationCenter.current().delegate = self
+        // 在程式一啟動即詢問使用者是否接受圖文(alert)、聲音(sound)、數字(badge)三種類型的通知
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+        }
+        // 註冊遠程通知
+        UIApplication.shared.registerForRemoteNotifications()
         return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceTokenString = deviceToken.reduce("") {
+            return $0 + String(format: "%02x", $1)
+        }
+        print("deviceTokenString: \(deviceTokenString)")
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("error: \(error)")
     }
 
     // MARK: UISceneSession Lifecycle
@@ -34,7 +53,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 使用者點選推播時觸發
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print(#function)
+        let content = response.notification.request.content
+        print(content.userInfo)
+        completionHandler()
+    }
+
+    // 讓 App 在前景也能顯示推播
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner])
+    }
+}
