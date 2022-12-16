8f9dae1372e49eaa64a2569b968856a8471357eb
Ying Hsun Chen <stoola20.526@gmail.com>
2022-12-16 17:24:41 +0800

    refactor: present image picker related alert with UIViewController extension

--- CookAndShare/Controller/NewPost/NewRecipeViewController.swift
+++ CookAndShare/Controller/NewPost/NewRecipeViewController.swift
@@ -60,33 +60,19 @@ class NewRecipeViewController: UIViewController {
     }
 
     func presentImagePicker() {
-        let controller = UIAlertController(title: "請選擇照片來源", message: nil, preferredStyle: .actionSheet)
-
-        let cameraAction = UIAlertAction(title: "相機", style: .default) { _ in
+        let cameraHandler: AlertActionHandler = { [weak self] _ in
+            guard let self = self else { return }
             self.imagePicker.sourceType = .camera
             self.present(self.imagePicker, animated: true)
         }
-        let photoLibraryAction = UIAlertAction(title: "相簿", style: .default) { _ in
+
+        let photoLibraryHandler: AlertActionHandler = { [weak self] _ in
+            guard let self = self else { return }
             self.imagePicker.sourceType = .photoLibrary
             self.present(self.imagePicker, animated: true)
         }
-        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
-        controller.addAction(cameraAction)
-        controller.addAction(photoLibraryAction)
-        controller.addAction(cancelAction)
 
-        if let popoverController = controller.popoverPresentationController {
-            popoverController.sourceView = self.view
-            popoverController.sourceRect = CGRect(
-                x: self.view.bounds.midX,
-                y: self.view.bounds.midY,
-                width: 0,
-                height: 0
-            )
-            popoverController.permittedArrowDirections = []
-        }
-
-        present(controller, animated: true, completion: nil)
+        presentImagePickerAlert(camaraHandler: cameraHandler, photoLibraryHandler: photoLibraryHandler)
     }
 
     @objc func addIngredient() {
--- CookAndShare/Extension/UIViewController+Ext.swift
+++ CookAndShare/Extension/UIViewController+Ext.swift
@@ -34,4 +34,28 @@ extension UIViewController {
 
         present(alert, animated: true)
     }
+
+    func presentImagePickerAlert(camaraHandler: ((UIAlertAction) -> Void)?, photoLibraryHandler: ((UIAlertAction) -> Void)?) {
+        let controller = UIAlertController(title: "請選擇照片來源", message: nil, preferredStyle: .actionSheet)
+
+        let cameraAction = UIAlertAction(title: "相機", style: .default, handler: camaraHandler)
+        let photoLibraryAction = UIAlertAction(title: "相簿", style: .default, handler: photoLibraryHandler)
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        controller.addAction(cameraAction)
+        controller.addAction(photoLibraryAction)
+        controller.addAction(cancelAction)
+
+        if let popoverController = controller.popoverPresentationController {
+            popoverController.sourceView = self.view
+            popoverController.sourceRect = CGRect(
+                x: self.view.bounds.midX,
+                y: self.view.bounds.midY,
+                width: 0,
+                height: 0
+            )
+            popoverController.permittedArrowDirections = []
+        }
+
+        present(controller, animated: true, completion: nil)
+    }
 }
